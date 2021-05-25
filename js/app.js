f3307729f7d0bb3af1d6a47ed4aed860d1b77f21
Your Name <ralphchristian.ortiz@gmail.com>
2021-05-25 18:19:44 +0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//Menu Icon Toggle
+const nav = document.querySelector(".nav-links");
+const menu = document.querySelector(".menu");
+const links = nav.querySelectorAll("a");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  menu.classList.toggle("toggle");
+});
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    menu.classList.toggle("toggle");
+  });
+});
+
+// Parallax Scroll
+(function () {
+  var parallax = $(".js-parallax");
+  if (parallax.length) {
+    parallax.each(function () {
+      var _this = $(this),
+        scale = _this.data("scale"),
+        orientation = _this.data("orientation");
+
+      new simpleParallax(_this[0], {
+        scale: scale,
+        orientation: orientation,
+        overflow: true,
+        delay: 0.6,
+        transition: "cubic-bezier(0,0,0,1)",
+      });
+    });
+  }
+})();
