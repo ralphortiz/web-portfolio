c258fc7d1ca4c10c82ab1b6ad4abb51655444342
Your Name <ralphchristian.ortiz@gmail.com>
2021-05-26 09:51:53 +0800

    update scroll behavior

--- js/app.js
+++ js/app.js
@@ -34,3 +34,15 @@ links.forEach(link => {
     });
   }
 })();
+
+$(document).on("click", 'a[href^="#"]', function (e) {
+  var id = $(this).attr("href");
+  var $id = $(id);
+  if ($id.length === 0) {
+    return;
+  }
+
+  e.preventDefault();
+  var pos = $id.offset().top;
+  $("body, html").animate({ scrollTop: pos });
+});
